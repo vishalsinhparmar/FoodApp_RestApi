6bc71d6c83a771ec0f306492b00fcd769e84c236
vishalsinh <vishalsinhparmar821@gamil.com>
2024-12-09 22:42:16 +0530

    add foodcategory and add sub category

---
+++ src/model/Admin/subCategory.model.js
@@ -0,0 +1,24 @@
+import mongoose  from "mongoose";
+
+const SubcategorySchema = new mongoose.Schema({
+    // coverimage:{
+    //     type:String,
+    //     require:true
+    // },
+    subCategoryname:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    count:{
+        type:Number,
+        default:1
+    }
+});
+
+const Subcaegory = mongoose.model('Subcaegory',SubcategorySchema)
+
+export default Subcaegory;
